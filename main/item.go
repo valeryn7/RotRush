c160d2ae6c1c4f570b177d1099af565213d7e613
valerynaterchocho@gmail.com <valerynaterchocho@gmail.com>
2025-08-20 00:29:15 -0300

    explosion de los items

--- main/item.go
+++ main/item.go
@@ -27,4 +27,21 @@ embedded_components {
   "  texture: \"/assets/items.atlas\"\n"
   "}\n"
   ""
+  position {
+    z: 1.0
+  }
+}
+embedded_components {
+  id: "fx"
+  type: "sprite"
+  data: "default_animation: \"explosion\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/items.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    z: 1.0
+  }
 }
